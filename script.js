7a3080fee9a6992a1f511e75a995b7a033c4a0f7
ErtanDogan <124527108+ErtanDogan@users.noreply.github.com>
2024-02-06 18:39:48 +0000

    Update script.js
    Added the button to increase the numbers and randomize color.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//empty
+main = document.querySelector(".main #content");
+main.textContent = "0 ";
+
+var counter = 0;
+var update = function(){
+  counter++;
+  main.textContent += counter + " ";
+  
+  if(counter >=  10){
+    window.clearInterval(timer);
+  }
+}
+
+function randomColor(){
+  return Math.floor(Math.random()*255);
+}
+
+var add = function(){
+  counter++;
+  main.textContent += counter + " ";
+  main.style.backgroundColor = "rgb(" + randomColor()+","+ randomColor()+","+ randomColor() + ")";
+}
+
+timer = window.setInterval(update, 500);
+button = document.querySelector("#button").addEventListener("click", add);
+
