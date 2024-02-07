4585a6af1eeb6526ee3c286e8227d424ee54780b
ErtanDogan <124527108+ErtanDogan@users.noreply.github.com>
2024-02-07 16:21:31 +0000

    Update script.js
    Added the button grow/shrink

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-main = document.querySelector(".main #content");
+var main = document.querySelector(".main #content");
 main.textContent = "0 ";
+
+var btn = document.querySelector("#button");
+btn.style.height = "20px";
+var target = 0;
+var keyPressed = false;
 
 var counter = 0;
 var update = function(){
@@ -12,15 +17,45 @@ var update = function(){
 }
 
 function randomColor(){
-  return Math.floor(Math.random()*255);
+  return Math.floor(Math.random()*100 + 155);
 }
 
 var add = function(){
   counter++;
   main.textContent += counter + " ";
   main.style.backgroundColor = "rgb(" + randomColor()+","+ randomColor()+","+ randomColor() + ")";
+  target = parseInt(btn.style.height) + 20;
+  if(!keyPressed){
+    grow();
+  } else {
+    shrink();
+  }
 }
 
-timer = window.setInterval(update, 500);
-button = document.querySelector("#button").addEventListener("click", add);
+var grow = function(){
+  btn.style.height = (parseInt(btn.style.height) + 1) + "px";
+  if(parseInt(btn.style.height) <= target){
+    window.requestAnimationFrame(grow);
+  }
+}
+var shrink = function(){
+  btn.style.height = parseInt(btn.style.height) - 2 + "px";
+  if(parseInt(btn.style.height) > 20){
+    window.requestAnimationFrame(shrink);
+  }
+}
+
+
+var timer = window.setInterval(update, 500);
+
+var button = document.querySelector("#button").addEventListener("click", add);
+var buttonReset = window.addEventListener("keydown", function(event){
+  if(event.which == 16)
+    keyPressed = true;
+});
+var buttonReset2 = window.addEventListener("keyup", function(event){
+  if(event.which == 16)
+    keyPressed = false;
+})
+
 
